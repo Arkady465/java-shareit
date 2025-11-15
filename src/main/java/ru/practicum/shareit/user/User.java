1b1f302103a8bf7f4e7b124f79d858b14e296130
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-11-15 22:49:51 +0300

    Add files via upload

---
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
